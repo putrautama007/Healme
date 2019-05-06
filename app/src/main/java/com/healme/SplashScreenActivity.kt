37451cdcb12fc309e64d5afafa236799cb65b2ae
Putra Utama <putrautama30@gmail.com>
2019-05-06 20:37:26 +0700

    Initial commit

---
+++ app/src/main/java/com/healme/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.healme
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
