1811d3738d29675924eb4683be4f66d2941a60e3
Putra Utama <putrautama30@gmail.com>
2019-05-07 20:54:38 +0700

    add map data

--- app/src/main/java/com/healme/ApotekLocationActivity.kt
+++ app/src/main/java/com/healme/ApotekLocationActivity.kt
@@ -2,6 +2,10 @@ package com.healme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.design.widget.BottomSheetBehavior
+import android.view.View
+import android.widget.LinearLayout
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,10 +13,31 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.bottom_sheet_apotek.*
+import org.jetbrains.anko.startActivity
 
-class ApotekLocationActivity : AppCompatActivity(), OnMapReadyCallback {
+class ApotekLocationActivity : AppCompatActivity(), OnMapReadyCallback, View.OnClickListener {
+    override fun onClick(v: View?) {
+        when(v){
+            btn_bottom_sheet -> {
+                if (bottomSheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                } else {
+                    bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                }
+            }
+            btn_direct ->{
+
+            }
+            btn_see_more ->{
+                startActivity<SeeMoreActivity>()
+            }
+        }
+    }
 
     private lateinit var mMap: GoogleMap
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+    private lateinit var param: LinearLayout.LayoutParams
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,17 +46,20 @@ class ApotekLocationActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        btn_bottom_sheet.setOnClickListener(this)
+        btn_see_more.setOnClickListener(this)
+        btn_direct.setOnClickListener(this)
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_container)
+        Handler().postDelayed(object : Runnable {
+            override fun run() {
+                bottomSheetBehavior.peekHeight =
+                        tv_apotek_name.measuredHeight + btn_bottom_sheet.measuredHeight + 350
+
+
+            }
+        }, 1)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
